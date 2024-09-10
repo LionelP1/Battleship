079498c0d908821eb2519b36a34b6ed9448973b7
Lionel <lpei1900@gmail.com>
2024-09-10 11:41:34 -0400

    Created a basic layout for the front page

---
+++ src/components/FrontPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function StartMenu() {
+
+  return (
+    <div className="start-menu">
+      <h1 className="title">Battle Ship</h1>
+      <h2 className="subheading">Classic Naval Combat</h2>
+      <button className="start-button">Start Game</button>
+    </div>
+  );
+}
