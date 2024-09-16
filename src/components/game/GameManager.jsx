d82a6b803d8c6da82f7a009877a2126d687467ce
Lionel <lpei1900@gmail.com>
2024-09-16 18:34:30 -0400

    Created components to handle starting game

---
+++ src/components/game/GameManager.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import ShipPlacement from './ShipPlacement';
+import Game from './Game';
+
+const GameManager = () => {
+
+
+  return (
+
+  );
+};
+
+export default GameManager;
