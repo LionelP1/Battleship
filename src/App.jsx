53f1206842b8cfb65f4c3befad361773e8082358
Lionel <lpei1900@gmail.com>
2024-10-21 15:44:39 -0400

    Fixed the background, added a border around popup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import GameManager from './components/game/GameManager';
 import StartMenu from './components/StartMenu';
+import backgroundImage from './assets/background.jpg';
 
 function App() {
   const [gameStarted, setGameStarted] = useState(false);
@@ -11,7 +12,11 @@ function App() {
   };
 
   return (
-    <div className="app">
+    <div className="app"
+      style={{ 
+          backgroundImage: `url(${backgroundImage})`
+      }}
+    >
       {gameStarted ? (
         <div className="game-wrapper">
           <GameManager />
@@ -26,3 +31,38 @@ function App() {
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+// import './App.css';
+// import GameManager from './components/game/GameManager';
+// import StartMenu from './components/StartMenu';
+// import backgroundImage from './assets/background.jpg'; // Import the image
+
+// function App() {
+//     const [gameStarted, setGameStarted] = useState(false);
+
+//     const handleStartGame = () => {
+//         setGameStarted(true);
+//     };
+
+//     return (
+//         <div 
+//             className="app" 
+//             style={{ 
+//                 backgroundImage: `url(${backgroundImage})` // Set the background image inline
+//             }}
+//         >
+//             {gameStarted ? (
+//                 <div className="game-wrapper">
+//                     <GameManager />
+//                 </div>
+//             ) : (
+//                 <div className="start-menu-wrapper">
+//                     <StartMenu onStart={handleStartGame} />
+//                 </div>
+//             )}
+//         </div>
+//     );
+// }
+
+// export default App;
